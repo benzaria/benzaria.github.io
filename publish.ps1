6f4f6b460810dccedd7905d9513ac1a66f3d6665
benzaria <benz.github@gmail.com>
2025-11-08 14:11:48 +0100

    push benzaria.github.io Saturday 8/11/25 - 2:11 PM

--- publish.ps1
+++ publish.ps1
@@ -8,5 +8,5 @@ $date = Get-Date -Format 'dddd d/M/yy - h:mm tt'
 git add .
 git commit -S -m "push benzaria.github.io $date"
 git remote add origin "https://benzaria@github.com/benzaria/benzaria.github.io"
-git pull origin main $_force
-git push origin main $_force
+git pull origin mucho $_force
+git push origin mucho $_force
