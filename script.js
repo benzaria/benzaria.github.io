398d62fc7e226a3a93e832d55462da0a19abada3
benzaria <benz.github@gmail.com>
2025-11-08 14:11:16 +0100

    push benzaria.github.io Saturday 8/11/25 - 2:11 PM

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
 
 const echo = console.log
+const Doc = document
 
 async function loadPages() {
     const user = 'benzaria'
-    const container = document.querySelector('.links')
+    const container = Doc.querySelector('.links')
 
     const res = await fetch(`https://api.github.com/users/${user}/repos`)
     const repos = await res.json()
 
-    repos.forEach(async (repo) => {
-        if (repo.name.toLowerCase() === location.hostname) return
+    repos.forEach(repo => {
+        const name = repo.name.toLowerCase()
+
+        if (name === 'campus') return // temporary change
+        if (name === location.hostname) return
         if (repo.has_pages === false) return
 
         echo(`adding: ${repo.name}`)
 
-        const a = document.createElement('a')
-        a.target = '_blank'
-        a.className = 'link-card'
-        a.href = `https://${user}.github.io/${repo.name}`
-        a.innerHTML = `
+        const card = Doc.createElement('a')
+        card.target = '_blank'
+        card.className = 'link-card'
+        card.href = `https://${user}.github.io/${name}`
+        card.innerHTML = `
             <i class="fa-solid fa-arrow-up-right-from-square"></i>
-             ${repo.name} 
+             ${name} 
             <i class="fa-solid fa-angle-right arrow-icon"></i>
         `
-        container.appendChild(a)
+        container.appendChild(card)
     })
 }
 
